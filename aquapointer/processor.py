480f6a3348c435ac4fa460d1073949c046c2a5fa
Misty-W <82074193+Misty-W@users.noreply.github.com>
2023-12-14 12:25:58 -0800

    Update `processor` attributes

--- aquapointer/processor.py
+++ aquapointer/processor.py
@@ -2,52 +2,37 @@ from typing import Dict
 from pulser import register
 
 
-class PulseSettings():
-    def __init__(
-        self,
-        brad,
-        omega,
-        duration,
-    ) -> None:
-
+class PulseSettings:
+    def __init__(self, brad, omega, pulse_duration, max_det) -> None:
         self.brad = brad
         self.omega = omega
-        self.duration = duration
+        self.pulse_duration = pulse_duration
+        self.max_det = max_det
 
 
-class Processor():
-    def __init__(
-        self,
-        device,
-        register: register
-    ) -> None:
-
+class Processor:
+    def __init__(self, device, register: register) -> None:
         self.register = register
         self.device = device
 
 
-
 class AnalogProcessor(Processor):
-
     def __init__(
         self,
         device,
         register: register,
         pulse_settings: PulseSettings,
     ) -> None:
-
         self.device = device
         self.register = register
         self.pulse_settings = pulse_settings
 
 
 class DigitalProcessor(Processor):
-
     def __init__(
         self,
         device,
         register: register,
     ) -> None:
-
         self.device = device
         self.register = register
