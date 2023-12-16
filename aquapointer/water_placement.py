59fa89f9ae5c07f8bf6c87e31370b16daa3b0403
Misty-W <82074193+Misty-W@users.noreply.github.com>
2023-12-15 21:07:34 -0800

    Fix bugs in `find_water_positions`

--- aquapointer/water_placement.py
+++ aquapointer/water_placement.py
@@ -4,8 +4,6 @@ import scipy
 
 sys.path.append("../aquapointer/")
 from density_mapping import (
-    find_index_of_excited_qubits,
-    find_positions_of_excited_qubits,
     rescaled_positions_to_3d_map,
 )
 from qubo_solution import default_cost, fit_gaussian, run_qubo
@@ -20,7 +18,6 @@ def find_water_positions(
     qubo_cost=default_cost,
     loc_from_bitstrings="k_means",
 ):
-    
     variance, amplitude = fit_gaussian(densities[0])
 
     bitstrings = []
@@ -36,10 +33,9 @@ def find_water_positions(
                 num_samples,
             )
         )
-    indexes = find_index_of_excited_qubits(bitstrings)
-    qubit_rescaled_positions = find_positions_of_excited_qubits(indexes)
+
     water_indices = rescaled_positions_to_3d_map(
-        qubit_rescaled_positions, loc_from_bitstrings
+        bitstrings, [p.scale_grid_to_register() for p in processor_configs]
     )
     water_positions = []
     # from the indices find the water molecule positions in angstroms
@@ -48,12 +44,4 @@ def find_water_positions(
         for idx_i, idx_j in slice:
             ls.append(points[i][idx_i, idx_j])
         water_positions.append(ls)
-    if loc_from_bitstrings == "k_means":
-        obs = scipy.cluster.vq.whiten(water_positions)
-        k_or_guess = len(water_positions)  # placeholder
-        final_positions = scipy.cluster.vq.kmeans(obs, k_or_guess)
-    else:
-        # can use user-defined function
-        final_positions = loc_from_bitstrings(water_positions)
-
-    return final_positions
+    return water_positions
