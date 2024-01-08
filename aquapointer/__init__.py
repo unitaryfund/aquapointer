58fee7aa14f148c277a294d61640c6e9d1fb0339
Misty-W <82074193+Misty-W@users.noreply.github.com>
2024-01-08 12:32:21 -0800

    Add copyright info to Python files
    Add copyright info to all Python files for analog part

--- aquapointer/__init__.py
+++ aquapointer/__init__.py
@@ -1,1 +1,7 @@
+# Copyright (C) Unitary Fund
+#
+# This source code is licensed under the GPL license (v3) found in the
+# LICENSE file in the root directory of this source tree.
+
+
 from aquapointer import density_mapping, processor, qubo_solution, water_placement
