b7e078dc7d92c3af5938a7fb7d354bfbafc44fce
Misty-W <82074193+Misty-W@users.noreply.github.com>
2024-01-29 18:36:03 -0800

    Add internal calculations for slicing utility

--- aquapointer/slicing.py
+++ aquapointer/slicing.py
@@ -15,18 +15,62 @@ def density_slices_by_plane(
 ) -> List[NDArray]:
     density_grid = Grid(filename)
     density_3d_array = density_grid.grid
-    origin = density_grid.origin
-    densities = [density_3d_array[x_slice, y_slice, z_slice]]
-    for s in range(len(slicing_planes) - 1):
-        plane1 = construct_plane(slicing_planes[s][0] + origin, slicing_planes[s][1])
-        plane2 = construct_plane(
-            slicing_planes[s + 1][0] + origin, slicing_planes[s + 1][1]
-        )
-        density = density_3d_array[x_slice, y_slice, z_slice]
-        densities.append(density)
-    density = density_3d_array[x_slice, y_slice, z_slice]
-    densities.append(density)
-    return densities
+    coords = np.array(density_grid.centers())
+    idx = [[] for _ in range(len(slicing_planes))]
+    coordinates = [[] for _ in range(len(slicing_planes))]
+    densities = [[] for _ in range(len(slicing_planes))]
+
+    for i in range(coords.shape[0]):
+        for j in range(coords.shape[1]):
+            for k in range(coords.shape[2]):
+                for s in range(len(slicing_planes) + 1):
+                    if (
+                        s == 0
+                    ):  # one slicing plane in opposite direction of the normal
+                        d = (coords[i, j, k] - slicing_planes[s - 1][0]).dot(
+                            slicing_planes[s - 1][1]
+                        )
+                        if d >= 0:
+                            idx[s].append(i, j, k)
+                            coordinates[s].append(
+                                coords[i, j, k] - d * slicing_planes[s - 1][1]
+                            )
+                            densities[s].append(density_3d_array[i, j, k])
+                            break
+
+                    elif s < len(slicing_planes):  # slice between two planes
+                        d1 = (coords[i, j, k] - slicing_planes[s - 1][0]).dot(
+                            slicing_planes[s - 1][1]
+                        )
+                        d2 = (coords[i, j, k] - slicing_planes[s][0]).dot(
+                            slicing_planes[s][1]
+                        )
+                        if d1 < 0 and d2 >= 0:
+                            idx[s].append(i, j, k)
+                            coordinates[s].append(
+                                coords[i, j, k]
+                                - (
+                                    d1 * slicing_planes[s - 1][1]
+                                    + d2 * slicing_planes[s][1]    
+                                )
+                                / 2
+                            )
+                            densities[s].append(density_3d_array[i, j, k])
+                            break
+
+                    else:  # slice with one plane, in direction of the normal
+                        d = (coords[i, j, k] - slicing_planes[s - 1][0]).dot(
+                            slicing_planes[s - 1][1]
+                        )
+                        if d < 0:
+                            idx[s].append(i, j, k)
+                            coordinates[s].append(
+                                coords[i, j, k] - d * slicing_planes[s - 1][1]
+                            )
+                            densities[s].append(density_3d_array[i, j, k])
+                            break
+
+    return coordinates, densities
 
 
 def density_slices_by_axis(
