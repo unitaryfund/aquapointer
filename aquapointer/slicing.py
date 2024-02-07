a399cb6e383478c253f887ee0b6d30e1cac75b89
Misty-W <82074193+Misty-W@users.noreply.github.com>
2024-02-06 19:14:33 -0800

    Shape lists of points and densities into arrays
    Shape lists of points and densities into arrays

--- aquapointer/slicing.py
+++ aquapointer/slicing.py
@@ -3,13 +3,14 @@
 # This source code is licensed under the GPL license (v3) found in the
 # LICENSE file in the root directory of this source tree.
 
+from itertools import groupby
 from typing import List, Tuple
 
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 from gridData import Grid
-from numpy.typing import NDArray
 from numpy.linalg import norm
+from numpy.typing import NDArray
 
 
 def density_file_to_grid(filename: str) -> Grid:
@@ -19,7 +20,8 @@ def density_file_to_grid(filename: str) -> Grid:
 def density_slices_by_axis(
     density_grid: Grid, axis: NDArray, distances: NDArray
 ) -> Tuple[List[NDArray]]:
-    slicing_planes = generate_planes_by_axis(axis, distances, density_grid.origin)
+    origin = density_grid.origin
+    slicing_planes = generate_planes_by_axis(axis, distances, origin)
     return density_slices_by_plane(density_grid, slicing_planes)
 
 
@@ -27,7 +29,7 @@ def density_slices_by_plane(
     density_grid: Grid, slicing_planes: List[Tuple[NDArray, NDArray]]
 ) -> Tuple[List[NDArray]]:
     idx_lists = [[] for _ in range(len(slicing_planes) + 1)]
-    point_lists= [[] for _ in range(len(slicing_planes) + 1)]
+    point_lists = [[] for _ in range(len(slicing_planes) + 1)]
     density_lists = [[] for _ in range(len(slicing_planes) + 1)]
     normals = [s / norm(s) for s in list(zip(*slicing_planes))[1]]
 
@@ -61,7 +63,45 @@ def density_slices_by_plane(
         point_lists[s].append(coords)
         density_lists[s].append(density)
 
-    return idx_lists, point_lists, density_lists
+    points = []
+    densities = []
+    for i in range(len(idx_lists)):
+        points_array, density_array = shape_slice(
+            point_lists[i], density_lists[i], normals[0]  # TODO: generalize
+        )
+        points.append(points_array)
+        densities.append(density_array)
+
+
+    return points, densities
+    # return idx_lists, point_lists, density_lists
+
+
+def shape_slice(points: NDArray, density, normal: NDArray):
+    # rotation angles between z' (normal) and the z-axis
+    theta = np.arctan2(normal[1], normal[0])
+    phi = np.arctan2(norm(normal[0:2]), normal[2])
+    # rotate x and y to x' and y' respectively
+    x_prime = np.array([np.cos(phi), np.sin(phi), 0])
+    y_prime = np.array(
+        [-np.sin(phi) * np.cos(theta), np.cos(phi) * np.cos(theta), np.sin(theta)]
+    )
+    # project points onto y' and group indices of projected points
+    point_list = []
+    density_list = []
+
+    for _, group in groupby(points, lambda x: x.dot(y_prime)):
+        # project points onto x' and sort indices of projected points
+        idxs_by_xp = np.argsort([g.dot(x_prime) for g in group])
+        point_list.append([points[i] for i in idxs_by_xp])
+        density_list.append([density[i] for i in idxs_by_xp])
+
+    m = max([len(p) for p in point_list])
+    n = len(point_list)
+    points_array = np.zeros((m, n))
+    density_array = np.zeros((m, n))
+
+    return points_array, density_array
 
 
 def generate_planes_by_axis(
