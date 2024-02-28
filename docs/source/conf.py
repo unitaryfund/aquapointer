2473e8e46cda3aa39b555d34751e3c991c49f4d2
Nathan Shammah <nathan.shammah@gmail.com>
2024-02-28 16:16:48 +0100

    move to docs folder

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,6 +5,10 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+
 
 project = 'aquapointer'
 copyright = '2024, Unitary Fund, Pasqal, and Qubit Pharmaceuticals'
