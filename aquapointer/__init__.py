ea3a08b0eddc3b7c7d224ed476704b056060e209
Misty-W <82074193+Misty-W@users.noreply.github.com>
2024-03-21 13:52:33 -0700

    Pre-req: fix cropping
    Fix cropping bug and update interface to work better with `DensityCanvas`
    
    Clean up slicing demo notebook

--- aquapointer/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright (C) Unitary Fund, Pasqal, and Qubit Pharmaceuticals.
-#
-# This source code is licensed under the GPL license (v3) found in the
-# LICENSE file in the root directory of this source tree.
-
-
-from aquapointer.analog import density_mapping, qubo_solution, water_placement, utils
-from aquapointer.analog_digital import processor
--- aquapointer/analog/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-# Copyright (C) Unitary Fund, Pasqal, and Qubit Pharmaceuticals.
-#
-# This source code is licensed under the GPL license (v3) found in the
-# LICENSE file in the root directory of this source tree.
-
-
-from aquapointer.analog.utils import benchmark_utils, density_utils, detuning_scale_utils
