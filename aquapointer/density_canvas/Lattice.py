b772c995582ef9bba0263a53236dc029ae20da97
Misty-W <82074193+Misty-W@users.noreply.github.com>
2024-03-21 14:47:58 -0700

    Formatting improvements

--- aquapointer/density_canvas/Lattice.py
+++ aquapointer/density_canvas/Lattice.py
@@ -13,60 +13,66 @@ from itertools import combinations
 from scipy.stats import qmc
 from scipy.integrate import RK45
 
-class Lattice:
-    """ This is a class that contains information on 2D lattices"""
 
-    def __init__(
-        self,
-        coords: ArrayLike,
-        **kwargs
-    ):
+class Lattice:
+    """This is a class that contains information on 2D lattices"""
+
+    def __init__(self, coords: ArrayLike, **kwargs):
         self._coords = np.array(coords, dtype=float)
         try:
-            self._min_spacing = kwargs['min_distance']
+            self._min_spacing = kwargs["min_distance"]
         except KeyError:
             self._min_spacing = emb.find_minimal_distance(self._coords)
         try:
-            self._length_x = kwargs['length_x']
+            self._length_x = kwargs["length_x"]
         except KeyError:
-            self._length_x = emb.find_maximal_distance(self._coords[:,0])
+            self._length_x = emb.find_maximal_distance(self._coords[:, 0])
         try:
-            self._length_y = kwargs['length_y']
+            self._length_y = kwargs["length_y"]
         except KeyError:
-            self._length_y = emb.find_maximal_distance(self._coords[:,1])
+            self._length_y = emb.find_maximal_distance(self._coords[:, 1])
         try:
-            self._center = kwargs['center']
+            self._center = kwargs["center"]
         except KeyError:
-            self._center = np.array([self._length_x/2, self._length_y/2])
+            self._center = np.array([self._length_x / 2, self._length_y / 2])
         try:
-            self._lattice_type = kwargs['lattice_type']
+            self._lattice_type = kwargs["lattice_type"]
         except KeyError:
             self._lattice_type = "custom"
 
     @classmethod
     def rectangular(cls, num_x: int, num_y: int, spacing: numbers.Number):
-        length_x = (num_x-1)*spacing
-        length_y = (num_y-1)*spacing
-        
+        length_x = (num_x - 1) * spacing
+        length_y = (num_y - 1) * spacing
+
         x_coords = []
         for i in range(num_x):
-            x_coords.append(i*spacing)
-        
+            x_coords.append(i * spacing)
+
         y_coords = []
         for i in range(num_y):
-            y_coords.append(i*spacing)
+            y_coords.append(i * spacing)
 
         coords = []
         for xc in x_coords:
             for yc in y_coords:
                 coords.append([xc, yc])
 
-        center = np.array([length_x/2, length_y/2])
-       
-        return cls(np.array(coords, dtype=float), min_spacing=spacing, length_x=length_x, length_y=length_y, center=center, type="rectangular")
+        center = np.array([length_x / 2, length_y / 2])
+
+        return cls(
+            np.array(coords, dtype=float),
+            min_spacing=spacing,
+            length_x=length_x,
+            length_y=length_y,
+            center=center,
+            type="rectangular",
+        )
 
     @classmethod
-    def poisson_disk(cls, density: ArrayLike, length: tuple, spacing: tuple, max_num: int = 8000):
+    def poisson_disk(
+        cls, density: ArrayLike, length: tuple, spacing: tuple, max_num: int = 8000
+    ):
         """
         Poisson disk sampling with variable radius.
         density: 2d array representing probability density
@@ -74,20 +80,20 @@ class Lattice:
         spacing: tuple (min_radius, max_radius) representing the minimum and maximum exclusion radius
         max_num: maximum number of points to sample
         """
-        
+
         min_radius, max_radius = spacing
         length_x, length_y = length
-        scale_x, scale_y = np.array(length)/np.array(density.shape)
+        scale_x, scale_y = np.array(length) / np.array(density.shape)
 
         def _index_from_position(pos):
-            idx_x = int((pos[1])/scale_x)
-            idx_y = int((pos[0])/scale_y)
+            idx_x = int((pos[1]) / scale_x)
+            idx_y = int((pos[0]) / scale_y)
             return (idx_x, idx_y)
-        
+
         # convert probability density map to a radius map
-        radius_density = -density #high probability = small radius and viceversa
+        radius_density = -density  # high probability = small radius and viceversa
         radius_density -= np.min(radius_density)
-        radius_density *= (max_radius-min_radius)/np.max(radius_density)
+        radius_density *= (max_radius - min_radius) / np.max(radius_density)
         radius_density += min_radius
         # now radius_density is a 2d array with values between max_radius and min_radius
 
@@ -96,13 +102,15 @@ class Lattice:
         num = 0
 
         # pick the first point as the density maximum and initialize queue
-        first_point = np.array([np.random.rand()*length_x, np.random.rand()*length_y])
+        first_point = np.array(
+            [np.random.rand() * length_x, np.random.rand() * length_y]
+        )
         coords.append(first_point)
         queue.append(num)
         num += 1
 
         # sample until max number is reached or points cannot be placed
-        while len(queue) and (num<max_num):
+        while len(queue) and (num < max_num):
             i = np.random.choice(queue)
             ref_point = coords[i]
             ref_radius = radius_density[_index_from_position(ref_point)]
@@ -112,11 +120,11 @@ class Lattice:
             tries = 0
             while (tries < 30) and not placed:
                 # pick a random point at random distance between [ref_radius, 2*ref_radius]
-                r = (np.random.rand()+1)*ref_radius
-                theta = 2*np.pi*np.random.rand()
-                new_point = np.array((r*np.cos(theta), r*np.sin(theta))) + ref_point
+                r = (np.random.rand() + 1) * ref_radius
+                theta = 2 * np.pi * np.random.rand()
+                new_point = np.array((r * np.cos(theta), r * np.sin(theta))) + ref_point
 
-                # burn a try if point falls outside space 
+                # burn a try if point falls outside space
                 if not (0 <= new_point[0] < length_x):
                     continue
                 if not (0 <= new_point[1] < length_y):
@@ -127,13 +135,13 @@ class Lattice:
                 # now check if new_point is too close to some other point based on new_radius
                 too_close = False
                 for other_point in coords:
-                    d = np.linalg.norm(new_point-other_point)
+                    d = np.linalg.norm(new_point - other_point)
                     if d < new_radius:
                         too_close = True
                         break
 
                 if too_close:
-                    tries +=1
+                    tries += 1
                 else:
                     coords.append(new_point)
                     queue.append(num)
@@ -144,16 +152,24 @@ class Lattice:
                 # after 30 tries, no point could be placed around i. remove it from the active queue
                 queue.remove(i)
 
-            center = np.array([length_x/2, length_y/2])
-        
+            center = np.array([length_x / 2, length_y / 2])
+
         return cls(np.array(coords, dtype=float), center=center, type="poisson_disk")
 
-
-    def dynamics(self, density: ArrayLike, length: tuple, spacing: numbers.Number, T: numbers.Number = 1000, dt: numbers.Number = 1, save_history=False, viscosity=0.1):
-        """ Calculates newtonian dynamics treating the lattice points as
+    def dynamics(
+        self,
+        density: ArrayLike,
+        length: tuple,
+        spacing: numbers.Number,
+        T: numbers.Number = 1000,
+        dt: numbers.Number = 1,
+        save_history=False,
+        viscosity=0.1,
+    ):
+        """Calculates newtonian dynamics treating the lattice points as
         particles subject to Lennard-Jones interactions plus a space dependent
         scalar field given by -density.
-        density: 2d array representing probability density, it is turned into attractive potential by putting a - sign in front 
+        density: 2d array representing probability density, it is turned into attractive potential by putting a - sign in front
         length: tuple (length_x, length_y) representing the physical size of the 2d space
         spacing: float indicating the minimal spacing between particles (minimum of Lennard-Jones potential)
         T: total time of the dynamics
@@ -162,72 +178,80 @@ class Lattice:
 
         def _leapfrog():
             t = 0
-            while t<T:
+            while t < T:
                 # half update velocities
                 for i in range(len(vs)):
-                    vs[i] += (_LennardJones_force(i) - _gradient(i) - _viscosity(i)) * dt/2
-                    #vs[i] += -_gradient(i) * dt/2
+                    vs[i] += (
+                        (_LennardJones_force(i) - _gradient(i) - _viscosity(i)) * dt / 2
+                    )
+                    # vs[i] += -_gradient(i) * dt/2
                 # update positions
                 for i in range(len(xs)):
                     xs[i] += vs[i] * dt
-                    if xs[i][0]>length[0]:
-                        xs[i][0]=length[0]
-                    if xs[i][0]<0:
-                        xs[i][0]=0
-                    if xs[i][1]>length[1]:
-                        xs[i][1]=length[1]
-                    if xs[i][1]<0:
-                        xs[i][1]=0
+                    if xs[i][0] > length[0]:
+                        xs[i][0] = length[0]
+                    if xs[i][0] < 0:
+                        xs[i][0] = 0
+                    if xs[i][1] > length[1]:
+                        xs[i][1] = length[1]
+                    if xs[i][1] < 0:
+                        xs[i][1] = 0
                     history[i].append(np.array(xs[i]))
                 # half update velocities
                 for i in range(len(vs)):
-                    vs[i] += (_LennardJones_force(i) - _gradient(i) - _viscosity(i)) * dt/2
-                    #vs[i] += -_gradient(i) * dt/2
+                    vs[i] += (
+                        (_LennardJones_force(i) - _gradient(i) - _viscosity(i)) * dt / 2
+                    )
+                    # vs[i] += -_gradient(i) * dt/2
                 t += dt
 
         def _viscosity(i):
-            return K*vs[i]
-        
+            return K * vs[i]
+
         def _gradient(i):
             idx_x, idx_y = _index_from_position(xs[i])
-            grad = np.gradient(attractive_field) 
-            field_contribution = np.array([grad[1][idx_x, idx_y], grad[0][idx_x, idx_y]])
+            grad = np.gradient(attractive_field)
+            field_contribution = np.array(
+                [grad[1][idx_x, idx_y], grad[0][idx_x, idx_y]]
+            )
             return field_contribution
-                
+
         def _index_from_position(pos):
-            idx_x = min(int((pos[1])/scale_x), density.shape[0]-1)
-            idx_y = min(int((pos[0])/scale_y), density.shape[1]-1)
+            idx_x = min(int((pos[1]) / scale_x), density.shape[0] - 1)
+            idx_y = min(int((pos[0]) / scale_y), density.shape[1] - 1)
             return (idx_x, idx_y)
-        
+
         def _position_from_index(idx):
-            return np.array([idx[1]*scale_x, idx[0]*scale_y])
-        
+            return np.array([idx[1] * scale_x, idx[0] * scale_y])
+
         def _LennardJones_force(i):
             LJ_force = np.zeros(2)
             # sum contribution of all other particles
             for j, _ in enumerate(xs):
-                if j==i:
+                if j == i:
                     continue
-                r_ij = xs[i]-xs[j]
+                r_ij = xs[i] - xs[j]
                 r = max(np.linalg.norm(r_ij), 1e-1)
                 # force is derivative of Lennard-Jones potential
-                LJ_force += A*(2*(sigma**2/r**3)-(sigma/r**2)) * r_ij
+                LJ_force += A * (2 * (sigma**2 / r**3) - (sigma / r**2)) * r_ij
             return LJ_force
-        
-        scale_x, scale_y = np.array(length)/np.array(density.shape)
-        
+
+        scale_x, scale_y = np.array(length) / np.array(density.shape)
+
         # define potential parameters
         attractive_field = -density
-        sigma = 12 * spacing/2 #lennard-jones minimal distance is 2**(1/6) sigma
-        A = 1e-4 * 4 * np.max(density) #the depth of the lennard-jones potential is A/4
-        K = viscosity #viscosity constant
-        
+        sigma = 12 * spacing / 2  # lennard-jones minimal distance is 2**(1/6) sigma
+        A = (
+            1e-4 * 4 * np.max(density)
+        )  # the depth of the lennard-jones potential is A/4
+        K = viscosity  # viscosity constant
+
         # initialize positions and velocities
         xs = self._coords
         vs = np.zeros(xs.shape)
-        #vs = .1*(2*np.random.rand(*xs.shape)-1)
+        # vs = .1*(2*np.random.rand(*xs.shape)-1)
 
-        # initialize history        
+        # initialize history
         history = []
         for x in xs:
             history.append([np.array(x)])
@@ -237,26 +261,8 @@ class Lattice:
 
         if save_history:
             self._history = history
-        
+
         self._min_spacing = emb.find_minimal_distance(self._coords)
-        self._length_x = emb.find_maximal_distance(self._coords[:,0])
-        self._length_y = emb.find_maximal_distance(self._coords[:,1])
+        self._length_x = emb.find_maximal_distance(self._coords[:, 0])
+        self._length_y = emb.find_maximal_distance(self._coords[:, 1])
         self._lattice_type = "dynamics"
-
-
-
-        
-        
-
-        
-
-
-
-
-
-
-
-
-
-
-
--- aquapointer/density_canvas/Lp_norm.py
+++ aquapointer/density_canvas/Lp_norm.py
@@ -2,8 +2,9 @@ import numpy as np
 from scipy.special import binom
 import itertools as it
 
+
 def integer_partitions(n):
-    """ Function to calculate integer partitions of n
+    """Function to calculate integer partitions of n
     (taken from stackoverflow)"""
     a = [0 for i in range(n + 1)]
     k = 1
@@ -19,12 +20,13 @@ def integer_partitions(n):
         while x <= y:
             a[k] = x
             a[l] = y
-            yield a[:k + 2]
+            yield a[: k + 2]
             x += 1
             y -= 1
         a[k] = x + y
         y = x + y - 1
-        yield a[:k + 1]
+        yield a[: k + 1]
+
 
 def partition_eqclasses_size(r):
     """Calculates the equivalence classes and returns their size.
@@ -36,7 +38,7 @@ def partition_eqclasses_size(r):
             continue
         eqclass_i = [r[i]]
         placed.append(i)
-        for j in range(i+1, len(r)):
+        for j in range(i + 1, len(r)):
             if j in placed:
                 continue
             if r[j] == r[i]:
@@ -44,6 +46,7 @@ def partition_eqclasses_size(r):
                 placed.append(j)
         res.append(eqclass_i)
     return [len(eq) for eq in res]
+
 
 def partition_symmetries(r):
     """Calculates the symmetries of partition r=(r_0, r_1, ...)
@@ -56,11 +59,13 @@ def partition_symmetries(r):
         res *= np.math.factorial(eqclasses_size[i])
     return res
 
+
 def integral(p, k, indices, powers, base, component, params):
-    res = base()**(p-k)
+    res = base() ** (p - k)
     for idx, pow in zip(indices, powers):
-        res *= component(idx, params)**pow
+        res *= component(idx, params) ** pow
     return float(res)
+
 
 def Lp_ising_energy(x, p, base, component, params, high=None, low=None):
     """Calculates the Lp ising energy of a set of binary variables x=[x_0, x_1, ..., x_n]
@@ -73,18 +78,18 @@ def Lp_ising_energy(x, p, base, component, params, high=None, low=None):
         low = 1
 
     res = 0
-    for k in range(1, p+1):
+    for k in range(1, p + 1):
         fact_k = np.math.factorial(k)
         res_k = 0
         partitions = integer_partitions(k)
         for r in partitions:
             # skip orders that you want to neglect
-            if len(r)<low or len(r)>high:
+            if len(r) < low or len(r) > high:
                 continue
 
             # calculate multiplicity of partition
-            multiplicity = fact_k/partition_symmetries(r)
-            
+            multiplicity = fact_k / partition_symmetries(r)
+
             # cycle over all permutations
             res_r = 0
             index_permutations = it.permutations(range(len(x)), r=len(r))
@@ -93,18 +98,19 @@ def Lp_ising_energy(x, p, base, component, params, high=None, low=None):
                 if np.any(x[indices] == 0):
                     continue
                 res_r += integral(p, k, indices, r, base, component, params)
-            res_k += multiplicity*res_r
-        res += (-1)**k * binom(p, k) * res_k
-            
+            res_k += multiplicity * res_r
+        res += (-1) ** k * binom(p, k) * res_k
+
     return res
+
 
 def Lp_ising_energy_second_order_truncation(x, p, base, component, params):
     """Calculates the Lp ising energy of a set of binary variables x=[x_0, x_1, ..., x_n]
     truncated to second order terms"""
-    
-    if p//2 != p/2:
+
+    if p // 2 != p / 2:
         raise ValueError("power p must be even")
-    
+
     res = 0
 
     # first order
@@ -112,9 +118,13 @@ def Lp_ising_energy_second_order_truncation(x, p, base, component, params):
         if x[i1] == 0:
             continue
         indices = [i1]
-        for k in range(1, p+1):
+        for k in range(1, p + 1):
             partition = [k]
-            res += binom(p,k) * (-1)**k * integral(p, k, indices, partition, base, component, params)
+            res += (
+                binom(p, k)
+                * (-1) ** k
+                * integral(p, k, indices, partition, base, component, params)
+            )
         # second order
         for i2 in range(len(x)):
             if i1 == i2:
@@ -124,42 +134,67 @@ def Lp_ising_energy_second_order_truncation(x, p, base, component, params):
 
             indices = [i1, i2]
             # k = 2j even
-            for j in range(1, p//2+1):
+            for j in range(1, p // 2 + 1):
                 partition = [j, j]
-                binom_even = binom(p, 2*j)
-                fact_even = np.math.factorial(2*j)
-                res += binom_even * fact_even/(2*np.math.factorial(j)**2) * integral(p, 2*j, indices, partition, base, component, params)
+                binom_even = binom(p, 2 * j)
+                fact_even = np.math.factorial(2 * j)
+                res += (
+                    binom_even
+                    * fact_even
+                    / (2 * np.math.factorial(j) ** 2)
+                    * integral(p, 2 * j, indices, partition, base, component, params)
+                )
                 for l in range(1, j):
-                    partition = [j-l, j+l]
-                    res += binom_even * fact_even/(np.math.factorial(j-l)*np.math.factorial(j+l)) * integral(p, 2*j, indices, partition, base, component, params)
+                    partition = [j - l, j + l]
+                    res += (
+                        binom_even
+                        * fact_even
+                        / (np.math.factorial(j - l) * np.math.factorial(j + l))
+                        * integral(
+                            p, 2 * j, indices, partition, base, component, params
+                        )
+                    )
             # k = 2j+1 odd
-            for j in range(1, (p-1)//2+1):
-                binom_odd = binom(p, 2*j+1)
-                fact_odd = np.math.factorial(2*j+1)
+            for j in range(1, (p - 1) // 2 + 1):
+                binom_odd = binom(p, 2 * j + 1)
+                fact_odd = np.math.factorial(2 * j + 1)
                 for l in range(j):
-                    partition = [j-l, j+l+1]
-                    res -= binom_odd * fact_odd/(np.math.factorial(j-l)*np.math.factorial(j+l+1)) * integral(p, 2*j+1, indices, partition, base, component, params)
+                    partition = [j - l, j + l + 1]
+                    res -= (
+                        binom_odd
+                        * fact_odd
+                        / (np.math.factorial(j - l) * np.math.factorial(j + l + 1))
+                        * integral(
+                            p, 2 * j + 1, indices, partition, base, component, params
+                        )
+                    )
 
     return res
+
 
 def Lp_ising_energy_first_order_truncation(x, p, base, component, params):
     """Calculates the Lp ising energy of a set of binary variables x=[x_0, x_1, ..., x_n]
     truncated to first order terms"""
-    
-    if p//2 != p/2:
+
+    if p // 2 != p / 2:
         print("Error: power p must be even")
         return
-    
+
     res = 0
 
     # first order
     for i1 in range(len(x)):
         if x[i1] == 0:
             continue
-        for k in range(1, p+1):
-            res += binom(p,k) * (-1)**k * integral(p, k, [i1], [k], base, component, params)
+        for k in range(1, p + 1):
+            res += (
+                binom(p, k)
+                * (-1) ** k
+                * integral(p, k, [i1], [k], base, component, params)
+            )
 
     return res
+
 
 def Lp_coefficients(M, p, base, component, params, high=None, low=None):
     """Calculates the Lp coefficients and stores them in a dictionary
@@ -170,7 +205,7 @@ def Lp_coefficients(M, p, base, component, params, high=None, low=None):
         high = p
     if low is None:
         low = 1
-    
+
     if high > p:
         print(f"Warning: calculating up to maximum degree {p}")
         high = p
@@ -180,54 +215,46 @@ def Lp_coefficients(M, p, base, component, params, high=None, low=None):
 
     # initialize every coefficient to 0
     coefficients = {}
-    for i in range(low, high+1):
+    for i in range(low, high + 1):
         coefficients[i] = {}
         idxs = it.combinations(range(M), i)
         for idx in idxs:
             coefficients[i][idx] = 0
 
     # calculate coefficients
-    for k in range(1, p+1):
+    for k in range(1, p + 1):
         fact_k = np.math.factorial(k)
         partitions = integer_partitions(k)
         for r in partitions:
             # skip orders that you want to neglect
-            if len(r)<low or len(r)>high:
+            if len(r) < low or len(r) > high:
                 continue
 
             # calculate multiplicity of partition
-            multiplicity = fact_k/partition_symmetries(r)
-            
+            multiplicity = fact_k / partition_symmetries(r)
+
             # cycle over all permutations
             index_permutations = it.permutations(range(M), r=len(r))
             for indices in index_permutations:
-                prefactor = (-1)**k * binom(p, k) * multiplicity 
-                value = integral(p, k, list(indices), r, base, component, params) 
-                coefficients[len(r)][tuple(sorted(indices))] += prefactor*value
+                prefactor = (-1) ** k * binom(p, k) * multiplicity
+                value = integral(p, k, list(indices), r, base, component, params)
+                coefficients[len(r)][tuple(sorted(indices))] += prefactor * value
     return coefficients
+
 
 def Lp_coefficients_first_order(M, p, base, component, params):
     """Calculates the first order Lp coefficients"""
-    
-    if p//2 != p/2:
+
+    if p // 2 != p / 2:
         raise ValueError("power p must be even")
-    
+
     # calculate first order
     coefficients = []
     for i1 in range(M):
         res = 0
-        for k in range(1, p+1):
-            prefactor = binom(p,k) * (-1)**k
+        for k in range(1, p + 1):
+            prefactor = binom(p, k) * (-1) ** k
             value = integral(p, k, [i1], [k], base, component, params)
             res += prefactor * value
         coefficients.append(res)
     return coefficients
-
-
-
-
-
-
-
-
-
